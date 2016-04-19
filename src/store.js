cc50259a471e0e57925b014b64fffe2bc6a515f4
Jean Yang <bdlas321@gmail.com>
2016-04-19 22:09:25 +0800

    redux-logger for logging redux state and actions

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
 import reducers from './reducers';
+
+const logger = createLogger({
+  level: 'info',
+  collapsed: true,
+  predicate: (getState, action) => true
+});
 
 let store = createStore(
   reducers,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
 );
 
 export default store;
